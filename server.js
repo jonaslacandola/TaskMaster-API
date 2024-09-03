9eb14c14d28621c590e70823dca8edc01d58d0a8
jlaca <jonaslacandola07@gmail.com>
2024-09-03 19:41:59 +0800

    some changes

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ connect(database).then(() => {
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`App running at port:${process.env.PORT | 3000}`);
+  console.log(`App running at port:${process.env.PORT || 3000}`);
 });
